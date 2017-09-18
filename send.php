47e7a4df69cd18cf167df699a283bccc7738e644
xenonk <xenon.k90@gmail.com>
2017-09-18 18:03:14 +0400

    Icon5

---
+++ send.php
@@ -0,0 +1,35 @@
+<?php
+if (isset($_POST['name'])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $tel = $_POST["tel"];
+    $message = $_POST["message"];
+
+    // E-mail address
+    $EmailTo = "email@to.com";
+    $Subject = "BEIJING	PLANETTRADING callback form";
+
+    $Body = "Name: ";
+    $Body .= $name;
+    $Body .= "\n";
+
+    $Body .= "Email: ";
+    $Body .= $email;
+    $Body .= "\n";
+
+    $Body .= "Tel: ";
+    $Body .= $tel;
+    $Body .= "\n";
+
+    $Body .= "Message: ";
+    $Body .= $message;
+    $Body .= "\n";
+
+    $success = mail($EmailTo, $Subject, $Body, "From:" . $email);
+
+    if ($success) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
