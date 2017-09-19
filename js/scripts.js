8e6010490a44ff5a4d2fc41d6982b2c181cfc26b
xenon_k <xenon.k90@gmail.com>
2017-09-19 18:44:34 +0400

    Update css

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,30 @@ $(function () {
 			data: "name=" + name + "&email=" + email + "&tel=" + tel + "&message=" + message,
 			success: function (text)
 			{
+				var status = $('#modal-status');
+
 				if (text == "success") {
+					if (status.hasClass('modal-error')) {
+						status.removeClass('modal-error')
+					}
+
 					$("#callback")[0].reset();
 					$('#modal').modal('show');
 					setTimeout(function ()
 					{
 						$("#modal").modal('hide');
 					}, 2500);
+
 				} else {
+					if (!status.hasClass('modal-error')) {
+						status.addClass('modal-error')
+					}
+
+					$('#modal').modal('show');
+					setTimeout(function ()
+					{
+						$("#modal").modal('hide');
+					}, 3500);
 				}
 			}
 		});
--- send.php
+++ send.php
@@ -28,7 +28,7 @@ if (isset($_POST['name'])) {
     $success = mail($EmailTo, $Subject, $Body, "From:" . $email);
 
     if ($success) {
-        echo "success";
+        echo "succûâàess";
     } else {
         echo "error";
     }
